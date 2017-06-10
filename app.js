ba1f9c1b1bf5c70eaaab0850cd92c3a5d1763186
Stekolschikov <stekolschikov@mail.ua>
2017-06-10 18:07:54 +0300

    screen

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var express = require('express'),
     path = require('path'),
     sassMiddleware = require('node-sass-middleware'),
     fs = require('fs'),
+    phantom = require('phantom'),
+    webshot = require('webshot'),
+    node_phantom = require('node-phantom'),
     db_chat = new sqlite3.Database('./chat/history.db');
 
 function getDirectories (srcpath) {
@@ -29,6 +32,10 @@ app.use(express.static('tpl'));
 
 
 app.use('/chat', require("./chat/index")(io, db_chat));
+app.use('/p', require("./screenshot/index")(io));
+
+
+
 
 // DEV
 fs.watch('tpl', {encoding: 'buffer'}, (eventType, filename) => { io.emit('reload'); });
---
+++ screenshot/index.js
@@ -0,0 +1,39 @@
+module.exports = function(io) {
+    let express = require('express'),
+      router  = express.Router();
+      var webshot = require('webshot');
+      var fs = require('fs');
+
+    router.get("/:url/:name/:mob", function (req, res) {
+      let url = req.params.url,
+          mob = req.params.mob,
+          name = req.params.name
+          options = '';
+          if(mob == "on") {
+            options = {
+                screenSize: {
+                  width: 320
+                , height: 480
+                }
+                , shotSize: {
+                    width: 320
+                  , height: 'all'
+                  }
+                , userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
+                    + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
+              };
+          } else
+            options = {}
+
+      var renderStream = webshot(url,options);
+      var file = fs.createWriteStream(`./screenshot/sreen/${name}.png`, {encoding: 'binary'});
+      renderStream.on('data', function(data) { file.write(data.toString('binary'), 'binary'); });
+      renderStream.on('end', function() {
+        file.close();
+        let s = fs.createReadStream(`./screenshot/sreen/${name}.png`);
+        res.setHeader('Content-Type', 'image/png');
+        s.pipe(res);
+      });
+    });
+    return router;
+}
